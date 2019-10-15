216e3b98674b7cf0b2b42b303d00dc582ac93ed0
Steven Inouye <steven.inouye91@gmail.com>
2019-10-15 02:19:45 -0700

    'asdf'

--- rpi-rgb-led-matrix/examples-api-use/rain.py
+++ rpi-rgb-led-matrix/examples-api-use/rain.py
@@ -1,7 +1,14 @@
 from PIL import Image
 import os
-size = 320,32
-im = Image.open("get.jpg")
+import sys
+
+size = 50,32
+if(len(sys.argv) < 2):
+	im = Image.open("get.jpg")
+else:
+	image_file = sys.argv[1]
+	im = Image.open(image_file)
+
 im.thumbnail(size)
 width, height = im.size
 left = (width - 32)/2
