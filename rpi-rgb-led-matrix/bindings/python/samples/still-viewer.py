4de5d288934f4aed8a8e0079f4443350630df2ca
Steven Inouye <steven.inouye91@gmail.com>
2019-10-15 22:34:44 -0700

    'asdf'

--- rpi-rgb-led-matrix/bindings/python/samples/still-viewer.py
+++ rpi-rgb-led-matrix/bindings/python/samples/still-viewer.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
-import time
-import sys
 from rgbmatrix import RGBMatrix, RGBMatrixOptions
 from PIL import Image
-import os
 import urllib2 as urllib
 import io
 from samplebase import SampleBase
@@ -12,7 +9,6 @@ class StillViewer(SampleBase):
     def __init__(self, *args, **kwargs):
         super(StillViewer, self).__init__(*args, **kwargs)
         self.parser.add_argument("-i", "--image", help="The image to display", default="../media/catnaps.jpg")
-
     
     def run(self):
         offset_canvas = self.matrix.CreateFrameCanvas()
@@ -25,6 +21,7 @@ class StillViewer(SampleBase):
             	im = Image.open(image_file).convert('RGB')
             else:
             	im = Image.open(sysArg).convert('RGB')
+            	
             im.thumbnail(size)
             width, height = im.size
             left = (width - 32)/2
@@ -33,19 +30,7 @@ class StillViewer(SampleBase):
             bottom = (height + 32)/2
             image = im.crop((left, top, right, bottom))
 
-
-            # options = RGBMatrixOptions()
-            # options.pixel_mapper_config = 'U-mapper'
-            # options.rows = 16
-            # options.cols = 32
-            # options.chain_length = 2
-            # options.parallel = 1
-            # options.hardware_mapping = 'regular'
-
-            # matrix = RGBMatrix(options = options)
-
             image.thumbnail((self.matrix.width, self.matrix.height), Image.ANTIALIAS)
-
             self.matrix.SetImage(image)
 
 # Main function
@@ -56,47 +41,3 @@ if __name__ == "__main__":
     still_viewer = StillViewer()
     if (not still_viewer.process()):
         still_viewer.print_help()
-
-
-
-# size = 50,32
-# if(len(sys.argv) < 2):
-# 	im = Image.open("../media/catnaps.jpg")
-# else:
-# 	sysArg = sys.argv[1]
-# 	if sysArg.find("http") >= 0 :
-# 		fd = urllib.urlopen(sysArg)
-# 		image_file = io.BytesIO(fd.read())
-# 		im = Image.open(image_file)
-# 	else:
-# 		im = Image.open(sysArg)
-
-# im.thumbnail(size)
-# width, height = im.size
-# left = (width - 32)/2
-# top = (height - 32)/2
-# right = (width + 32)/2
-# bottom = (height + 32)/2
-# image = im.crop((left, top, right, bottom))
-
-
-# options = RGBMatrixOptions()
-# options.pixel_mapper_config = 'U-mapper'
-# options.rows = 16
-# options.cols = 32
-# options.chain_length = 2
-# options.parallel = 1
-# options.hardware_mapping = 'regular'
-
-# matrix = RGBMatrix(options = options)
-
-# image.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
-
-# matrix.SetImage(image.convert('RGB'))
-
-# try:
-#     print("Press CTRL-C to stop.")
-#     while True:
-#         time.sleep(100)
-# except KeyboardInterrupt:
-#     sys.exit(0)
