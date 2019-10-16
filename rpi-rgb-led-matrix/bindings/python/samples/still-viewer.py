1bed5444016289b7916174734f5a707fc488ab3b
Steven Inouye <steven.inouye91@gmail.com>
2019-10-15 22:04:58 -0700

    'asdf'

--- rpi-rgb-led-matrix/bindings/python/samples/still-viewer.py
+++ rpi-rgb-led-matrix/bindings/python/samples/still-viewer.py
@@ -19,7 +19,7 @@ class StillViewer(SampleBase):
         while True:
             size = 50,32
             # if(len(sys.argv) < 2):
-            # 	im = Image.open("../media/catnaps.jpg")
+        	im = Image.open("../media/catnaps.jpg")
             # else:
             # 	sysArg = sys.argv[1]
             # 	if sysArg.find("http") >= 0 :
@@ -29,13 +29,13 @@ class StillViewer(SampleBase):
             # 	else:
             # 		im = Image.open(sysArg)
 
-            # im.thumbnail(size)
-            # width, height = im.size
-            # left = (width - 32)/2
-            # top = (height - 32)/2
-            # right = (width + 32)/2
-            # bottom = (height + 32)/2
-            # image = im.crop((left, top, right, bottom))
+            im.thumbnail(size)
+            width, height = im.size
+            left = (width - 32)/2
+            top = (height - 32)/2
+            right = (width + 32)/2
+            bottom = (height + 32)/2
+            image = im.crop((left, top, right, bottom))
 
 
             # options = RGBMatrixOptions()
@@ -48,9 +48,9 @@ class StillViewer(SampleBase):
 
             # matrix = RGBMatrix(options = options)
 
-            # image.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
+            image.thumbnail((self.matrix.width, self.matrix.height), Image.ANTIALIAS)
 
-            # matrix.SetImage(image.convert('RGB'))
+            matrix.SetImage(image.convert('RGB'))
 
 # Main function
 # e.g. call with
