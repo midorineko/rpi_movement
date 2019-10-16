3a9624ed591a3feca88fcec6537280d4b77e5109
Steven Inouye <steven.inouye91@gmail.com>
2019-10-15 21:49:34 -0700

    'asdf'

--- rpi-rgb-led-matrix/bindings/python/samples/still-viewer.py
+++ rpi-rgb-led-matrix/bindings/python/samples/still-viewer.py
@@ -16,25 +16,40 @@ class StillViewer(SampleBase):
         self.parser.add_argument("-i", "--image", help="The image to display", default="../../../examples-api-use/runtext.ppm")
 
     def run(self):
-        if not 'image' in self.__dict__:
-            self.image = Image.open(self.args.image).convert('RGB')
-        self.image.resize((self.matrix.width, self.matrix.height), Image.ANTIALIAS)
+        size = 50,32
+        if(len(sys.argv) < 2):
+        	im = Image.open("../media/catnaps.jpg")
+        else:
+        	sysArg = sys.argv[1]
+        	if sysArg.find("http") >= 0 :
+        		fd = urllib.urlopen(sysArg)
+        		image_file = io.BytesIO(fd.read())
+        		im = Image.open(image_file)
+        	else:
+        		im = Image.open(sysArg)
 
-        double_buffer = self.matrix.CreateFrameCanvas()
-        img_width, img_height = self.image.size
+        im.thumbnail(size)
+        width, height = im.size
+        left = (width - 32)/2
+        top = (height - 32)/2
+        right = (width + 32)/2
+        bottom = (height + 32)/2
+        image = im.crop((left, top, right, bottom))
 
-        # let's scroll
-        xpos = 0
-        while True:
-            xpos += 1
-            if (xpos > img_width):
-                xpos = 0
 
-            double_buffer.SetImage(self.image, -xpos)
-            double_buffer.SetImage(self.image, -xpos + img_width)
+        options = RGBMatrixOptions()
+        options.pixel_mapper_config = 'U-mapper'
+        options.rows = 16
+        options.cols = 32
+        options.chain_length = 2
+        options.parallel = 1
+        options.hardware_mapping = 'regular'
 
-            double_buffer = self.matrix.SwapOnVSync(double_buffer)
-            time.sleep(0.01)
+        matrix = RGBMatrix(options = options)
+
+        image.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
+
+        matrix.SetImage(image.convert('RGB'))
 
 # Main function
 # e.g. call with
