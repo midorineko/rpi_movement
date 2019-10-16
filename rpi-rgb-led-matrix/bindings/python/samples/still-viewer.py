840baef9bb1f7d8e8146e9dd3f954e660fc7f2b0
Steven Inouye <steven.inouye91@gmail.com>
2019-10-15 22:02:08 -0700

    'asdf'

--- rpi-rgb-led-matrix/bindings/python/samples/still-viewer.py
+++ rpi-rgb-led-matrix/bindings/python/samples/still-viewer.py
@@ -17,40 +17,40 @@ class StillViewer(SampleBase):
     def run(self):
         offset_canvas = self.matrix.CreateFrameCanvas()
         while True:
-            size = 50,32
-            if(len(sys.argv) < 2):
-            	im = Image.open("../media/catnaps.jpg")
-            else:
-            	sysArg = sys.argv[1]
-            	if sysArg.find("http") >= 0 :
-            		fd = urllib.urlopen(sysArg)
-            		image_file = io.BytesIO(fd.read())
-            		im = Image.open(image_file)
-            	else:
-            		im = Image.open(sysArg)
+            # size = 50,32
+            # if(len(sys.argv) < 2):
+            # 	im = Image.open("../media/catnaps.jpg")
+            # else:
+            # 	sysArg = sys.argv[1]
+            # 	if sysArg.find("http") >= 0 :
+            # 		fd = urllib.urlopen(sysArg)
+            # 		image_file = io.BytesIO(fd.read())
+            # 		im = Image.open(image_file)
+            # 	else:
+            # 		im = Image.open(sysArg)
 
-            im.thumbnail(size)
-            width, height = im.size
-            left = (width - 32)/2
-            top = (height - 32)/2
-            right = (width + 32)/2
-            bottom = (height + 32)/2
-            image = im.crop((left, top, right, bottom))
+            # im.thumbnail(size)
+            # width, height = im.size
+            # left = (width - 32)/2
+            # top = (height - 32)/2
+            # right = (width + 32)/2
+            # bottom = (height + 32)/2
+            # image = im.crop((left, top, right, bottom))
 
 
-            options = RGBMatrixOptions()
-            options.pixel_mapper_config = 'U-mapper'
-            options.rows = 16
-            options.cols = 32
-            options.chain_length = 2
-            options.parallel = 1
-            options.hardware_mapping = 'regular'
+            # options = RGBMatrixOptions()
+            # options.pixel_mapper_config = 'U-mapper'
+            # options.rows = 16
+            # options.cols = 32
+            # options.chain_length = 2
+            # options.parallel = 1
+            # options.hardware_mapping = 'regular'
 
-            matrix = RGBMatrix(options = options)
+            # matrix = RGBMatrix(options = options)
 
-            image.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
+            # image.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
 
-            matrix.SetImage(image.convert('RGB'))
+            # matrix.SetImage(image.convert('RGB'))
 
 # Main function
 # e.g. call with
